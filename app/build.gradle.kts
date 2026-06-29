6d3e98a5ebef1dea1326fc4d4434fdb4b0ad41f9
thienhanbaro <lethiennhanvip.vn@gmail.com>
2026-06-29 13:55:11 +0700

    Change Shizuku dependencies to version 12.2.0
    Updated Shizuku API and provider dependencies to version 12.2.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("dev.rikka.shizuku:api:13.1.5")
-    implementation("dev.rikka.shizuku:provider:13.1.5")
+    implementation("dev.rikka.shizuku:api:12.2.0")
+    implementation("dev.rikka.shizuku:provider:12.2.0")
 }
